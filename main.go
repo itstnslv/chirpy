cb0bd5e2cccf733e0a89ceaf10260afaa8a010c6
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-10-21 22:28:23 +0500

    First custom handler

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			return
+		}
+	})
 
 	server := &http.Server{
 		Handler: mux,
