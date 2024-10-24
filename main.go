03c9a8b3cfb9d26957338668ed0bd8d997327faa
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-10-24 09:30:37 +0500

    Metrics refactoring

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -9,19 +8,6 @@ import (
 
 type apiConfig struct {
 	fileServerHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileServerHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) middlewareMetricReport(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("<html>\n  <body>\n    <h1>Welcome, Chirpy Admin</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>", cfg.fileServerHits.Load())))
 }
 
 func main() {
